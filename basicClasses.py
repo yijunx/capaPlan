3188ad3e717d4b7f45e85719a79601953798127b
xuerjun <yijunx@umich.edu>
2018-04-14 16:18:06 +0800

    first a few lines and examples

--- basicClasses.py
+++ basicClasses.py
@@ -9,7 +9,15 @@ import pandas as pd
 import numpy as np
 
 
+class Workstation:
+    def __init__(self, name, util: '0.67 for example'):
+        self.name = name
+        self.util = util
+        self.stepMatrix = pd.DataFrame(columns=['step name'])
 
+    #def map_a_step(self, stepname, rpt, loadsize: 'use 1 for single wafer', util: 'put 0 if using WS util', inflow):
+    #    self.stepMatrix.append()
+    #    return 0
 
-
-
+    def map_steps_from_csv(self, csv_link):
+        self.stepMatrix = pd.read_csv(csv_link, header=0)
